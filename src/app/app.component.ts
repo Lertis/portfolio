54655933d78cc518e74b66278041f2cedf2a5d09
Yura Husak <yura.gusak7@gmail.com>
2022-08-11 14:23:15 +0300

    Added arrow down animation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -58,18 +58,6 @@ export class AppComponent implements OnInit, AfterViewInit {
         }
       })
     })
-    /*     const sections = gsap.utils.toArray(".panel")
-        gsap.to(sections, {
-          xPercent: -100 * (sections.length - 1),
-          ease: "none",
-          scrollTrigger: {
-            trigger: ".container",
-            pin: true,
-            scrub: 1,
-            snap: 1 / (sections.length - 1),
-            end: "+=3500",
-          }
-        }) */
   }
 
   private readonly registerGsapPlugins = () => gsap.registerPlugin(ScrollTrigger)
